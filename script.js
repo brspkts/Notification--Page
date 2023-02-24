c78a7867db76f55d9e44508a902b53aaa521fe1c
brspkts <122319070+brspkts@users.noreply.github.com>
2023-02-24 15:39:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+function myFunction(){
+    document.getElementById("notif").style.display = "none";
+    document.getElementById("chatbox1").style.backgroundColor = "hsl(0, 0%, 100%)";
+    document.getElementById("chatbox2").style.backgroundColor = "hsl(0, 0%, 100%)";
+    document.getElementById("chatbox3").style.backgroundColor = "hsl(0, 0%, 100%)";
+    document.getElementById("dot1").style.display = "none";
+    document.getElementById("dot2").style.display = "none";
+    document.getElementById("dot3").style.display = "none";
+}
